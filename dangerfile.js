e4b825bf724b88fd3e4feda6de84df33c5c6d184
Noah Benham <noah.benham@cerner.com>
2018-06-27 12:11:39 -0500

    Update to node 8.11.3

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars
-import { danger, warn, fail, message } from 'danger';
+import {
+  danger, warn, fail,
+} from 'danger';
 
 const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
   const srcFilePattern = /CHANGELOG.md/i;
